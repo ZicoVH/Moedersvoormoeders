54f765f7119ff4cef3f4da87de455a1969a5d377
Zico Vanheuckelom <71635898+ZicoVH@users.noreply.github.com>
2022-04-27 13:31:45 +0200

    start push

---
+++ js/mvm.js
@@ -0,0 +1,39 @@
+(function ($) {
+    $(function () {
+
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+});
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.collapsible').collapsible();
+});
+
+
+
+// voor navbar op mobile
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
